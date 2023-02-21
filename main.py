ef868eba57da5bab620536a33bf50477dc9c4ae7
edmuntremmy <edmantas2010@yandex.ru>
2023-02-21 12:22:57 +0300

    delete

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ print("Введите коэффициенты для уравнения")
 print("ax^2 + bx + c = 0:")
 print()
 
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))
+
 
 discr = b ** 2 - 4 * a * c
 print("Дискриминант D = %.2f" % discr)
